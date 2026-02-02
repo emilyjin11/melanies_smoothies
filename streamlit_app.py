ea2670673ddf1b638c3acb3023b6ce0a4770b925
Emily J <77410222+emilyjin11@users.noreply.github.com>
2026-02-02 14:54:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -47,6 +49,5 @@ if ingredients_list:
 
         st.success('Your smoothie is ordered!')
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response).json()
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+
